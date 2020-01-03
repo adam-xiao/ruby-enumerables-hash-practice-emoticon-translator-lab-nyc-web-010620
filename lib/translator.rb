7b89941eb3a2d1e6713b339cdf7cec3f7c8af4d4
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-03 09:53:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,8 @@ require 'yaml'
 def load_library(file_path)
   emoticon_list = YAML.load_file(file_path)
   p emoticon_list
-  meaning_hash = {"get_meaning": {}}
-  emoticon_hash = {"get_emoticon": {}}
+  meaning_hash = {"get_meaning": emoticon_list[emotion][1]}
+  emoticon_hash = {"get_emoticon": emoticon_list[emotion][0]}
 end
 
 def get_japanese_emoticon
