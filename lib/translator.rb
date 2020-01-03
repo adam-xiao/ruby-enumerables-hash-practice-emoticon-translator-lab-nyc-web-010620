cdb1545da99c8532fe2bb758fa5b222f97d1f262
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-03 17:43:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,9 +18,9 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  load_library(file_path)
+  emoticon_list = load_library(file_path)
 
-  emoticon_hash[:get_emoticon][emoticon]
+  emoticon_list[:get_emoticon][emoticon]
 
 end
 
