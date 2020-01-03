b7f19be7e4339d54b71ce25867d3586864d70551
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-03 09:38:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,9 +2,9 @@
 require 'yaml'
 
 def load_library
-  
-emoticon_list = YAML.load_file('emoticon.yml')
-puts emoticon_list.inspect
+
+  emoticon_list = YAML.load_file('emoticon.yml')
+  puts emoticon_list.inspect
 end
 
 def get_japanese_emoticon
